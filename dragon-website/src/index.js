14bac91107a61c6201e1a56fe48e23b3a6ab7f5b
Hongyi Tian <russell.tian1996@gmail.com>
2019-08-18 00:58:30 -0400

    copy the layout from react mdl

--- dragon-website/src/index.js
+++ dragon-website/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//Copied from youtube
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
